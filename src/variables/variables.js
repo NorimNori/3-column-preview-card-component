4377beed6f70f4fee4d417e588160581f8c7b08a
Gloria R <norimaqidoe@hotmail.com>
2025-02-12 22:15:53 -0600

    feat: add variable for information rendering

---
+++ src/variables/variables.js
@@ -0,0 +1,27 @@
+import sedanIcon from "@/assets/imagenes/sedan.svg";
+import suvIcon from "@/assets/imagenes/suv.svg";
+import luxuryIcon from "@/assets/imagenes/luxury.svg";
+
+export const CAR_CATEGORIES = [
+  {
+    id: 1,
+    title: "SEDANS",
+    description: "Choose a sedan for its affordability and fuel economy.",
+    icon: sedanIcon,
+  },
+  {
+    id: 2,
+    title: "SUVS",
+    description: "Spacious interior, power, and versatility.",
+    icon: suvIcon,
+  },
+  {
+    id: 3,
+    title: "LUXURY",
+    description: "Enhanced comfort of a luxury rental.",
+    icon: luxuryIcon,
+  },
+];
+
+  
+  
