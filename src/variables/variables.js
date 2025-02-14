92e0aa2ca931b3869cbcb06096c0833b9b4e2548
Gloria R <norimaqidoe@hotmail.com>
2025-02-13 21:44:58 -0600

    fix: add image import for correct deploy

--- src/variables/variables.js
+++ src/variables/variables.js
@@ -1,3 +1,6 @@
+import sedanIcon from '@/assets/images/icon-sedans.svg';
+import suvIcon from '@/assets/images/icon-suvs.svg';
+import luxuryIcon from '@/assets/images/icon-luxury.svg';
 
 export const CAR_CATEGORIES = [
   {
@@ -5,21 +8,21 @@ export const CAR_CATEGORIES = [
     title: "SEDANS",
     description: "Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.",
     color: "$brightOrange",
-    icon: "./src/assets/images/icon-sedans.svg",
+    icon: sedanIcon,
   },
   {
     id: 2,
     title: "SUVS",
     description: "Take an SUV fot its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.",
     color: "$darkCyan",
-    icon: "./src/assets/images/icon-suvs.svg",
+    icon: suvIcon,
   },
   {
     id: 3,
     title: "LUXURY",
     description: "Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.",
     color: "$veryDarkCyan",
-    icon: "./src/assets/images/icon-luxury.svg",
+    icon: luxuryIcon,
   },
 ];
 
